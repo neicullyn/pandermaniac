899063261ca2544e3160aec734dc5cae864c757f
minfawan <minfa.wang@gmail.com>
2015-02-25 21:27:45 -0800

    modification on platform

--- platform.py
+++ platform.py
@@ -104,27 +104,40 @@ if __name__ == '__main__':
     MAX_ITE = 1
     NUM_DEFAULT = 4
     NUM_WINNING = 8
-    NUM_MUTATIONS = 8
-    NUM_RANDOMS = 8
-    NUM_STRATEGIES = NUM_DEFAULT + NUM_WINNING + NUM_MUTATIONS + NUM_RANDOMS
+    NUM_MUTATIONS = 4
+    NUM_RANDOMS = 4
+    NUM_STRATEGIES = NUM_WINNING + NUM_MUTATIONS + NUM_RANDOMS + NUM_DEFAULT
     map_info = load_maps('test_maps.txt')
     map_info_dict = {}
     for one_map_info in map_info:
         map_info_dict[one_map_info[0]] = one_map_info
 
+    
+    # for file_name in default_strategies_files:
+    #     s = strategy.Strtg()
+    #     s.read_from_file(os.path.join('training', 'default_strategies', file_name))
+    #     default_strategies.append(s) 
+        
     # load default strategies from the folder
     default_strategies = []
-    default_strategies_files = os.listdir(os.path.join('training', 'default_strategies'))
-    
-    for file_name in default_strategies_files:
+    default_nodes_list = []
+    dft_stg_dir_name = os.path.join('training', 'default_strategies')
+    default_strategies_dir = os.listdir(dft_stg_dir_name)
+    for file_name in default_strategies_dir:
         s = strategy.Strtg()
-        s.read_from_file(os.path.join('training', 'default_strategies', file_name))
-        default_strategies.append(s) 
-        
+        s.read_from_file(os.path.join(dft_stg_dir_name, file_name))
+        default_strategies.append(s)
+        default_nodes = {}
+        for one_map_info in map_info:
+            map_name, map_data, json_dict, n_players, n_seeds = one_map_info
+            default_nodes[map_name] = s.get_nodes(map_data, n_players, n_seeds)
+        default_nodes_list.append(default_nodes)
+
+
 
     last_step_strategies = {}
 
-    # ************ modify so IT_NUM in range(MX_CUR, MX_ITE) ************
+
     steps_dir = os.path.join('training', 'iterations')
     ite_folders = [int(ite_folder) for ite_folder in os.listdir(steps_dir) if ite_folder.isdigit()]
     if len(ite_folders) == 1:
@@ -132,7 +145,6 @@ if __name__ == '__main__':
     else:
         ite_folders.sort(reverse=True)
         ITE_CUR = ite_folders[1]
-
 
     for ITE_NUM in range(ITE_CUR, MAX_ITE):
 
@@ -147,16 +159,8 @@ if __name__ == '__main__':
             # last_step_strategies.append(s)
             stg_idx = int(file_name[:-5])
             last_step_strategies[stg_idx] = s
-        
-        # for s in last_step_strategies:
-        #     print s
-        
-        # map_name, map_data, json_dict, n_players, n_seeds = map_info[1]
-        # nodes1 = last_step_strategies[0].get_nodes(map_data, n_players, n_seeds)
-        # nodes2 = last_step_strategies[1].get_nodes(map_data, n_players, n_seeds)
-        
-        # nodes = {'strategy1': [nodes1],
-        #          'strategy2': [nodes2]}
+
+
 
 
         # --------------------- Minfa edited begins ---------------------
@@ -175,11 +179,11 @@ if __name__ == '__main__':
                     # map_score += map score with randomly chosen competitors
                 # rank_dict[stg_idx] = map_score
         # choose 16 stg_idx from rank_dict with highest map_score
-        stg_nodes = {}
         stg_nodes_dict = {}
         rank_dict = {}
 
         # stg_idx will range from 0 to NUM_WINNING
+        stg_nodes = {}
         for stg_idx, stg in last_step_strategies.items():
             for one_map_info in map_info:
                 map_name, map_data, json_dict, n_players, n_seeds = one_map_info
@@ -189,84 +193,88 @@ if __name__ == '__main__':
         # add more strategies here
         # mutations
         # randoms
+        mut_stg_nodes = {}
         last_step_stg_list = last_step_strategies.values()
         mutate_group = strategy.generate_mutation_group(last_step_stg_list, NUM_MUTATIONS)
         for stg_idx in range(NUM_MUTATIONS):
             mut_stg_idx = stg_idx + NUM_WINNING
-            stg_nodes_dict[mut_stg_idx] = mutate_group[stg_idx]
+            mut_s = mutate_group[stg_idx]
+            for one_map_info in map_info:
+                map_name, map_data, json_dict, n_players, n_seeds = one_map_info
+                mut_stg_nodes[map_name] = mut_s.get_nodes(map_data, n_players, n_seeds)
+            stg_nodes_dict[mut_stg_idx] = mut_stg_nodes
+            last_step_strategies[mut_stg_idx] = mut_s
 
-        # random_group = []
+
+        rand_stg_nodes = {}
         for stg_idx in range(NUM_RANDOMS):
-            rand_stg_idx = stg_idx + NUM_WINNING + NUM_RANDOMS
+            rand_stg_idx = stg_idx + NUM_WINNING + NUM_MUTATIONS
             s = strategy.Strtg()
             s.random_weight(strategy.preprocess({}))
-            # random_group.append(s)
-            stg_nodes_dict[rand_stg_idx] = s
+            for one_map_info in map_info:
+                map_name, map_data, json_dict, n_players, n_seeds = one_map_info
+                rand_stg_nodes[map_name] = s.get_nodes(map_data, n_players, n_seeds)
+            stg_nodes_dict[rand_stg_idx] = rand_stg_nodes
+            last_step_strategies[rand_stg_idx] = s
 
 
-        # for key, value in stg_nodes_dict.items():
-            # print value
+        # default_strategies = [s]
+        # default_nodes = {map_name: nodes}
+        # default_nodes_list = [default_nodes]
+        for stg_idx in range(NUM_DEFAULT):
+            dft_stg_idx = stg_idx + NUM_WINNING + NUM_MUTATIONS + NUM_RANDOMS
+            stg_nodes_dict[dft_stg_idx] = default_nodes_list[stg_idx]
+            last_step_strategies[dft_stg_idx] = default_strategies[stg_idx]
 
-    #     for stg_idx, stg_nodes in stg_nodes_dict.items():
-    #         map_score = 0
-    #         for map_name, self_nodes_chosen in stg_nodes.items():
-    #             nodes = {}
-    #             nodes[stg_idx] = [self_nodes_chosen]
+
+
+        for stg_idx, stg_nodes in stg_nodes_dict.items():
+            map_score = 0
+            for map_name, self_nodes_chosen in stg_nodes.items():
+                nodes = {}
+                nodes[stg_idx] = [self_nodes_chosen]
                 
 
-    #             # make other_nodes_chosen
-    #             play_map_info = map_info_dict[map_name]
-    #             map_name, map_data, json_dict, n_players, n_seeds = play_map_info
-    #             # other_nodes_chosen_list = []
-    #             players_in_game = {stg_idx}
-    #             for one_player in range(n_players-1):
-    #                 rand_idx = random.randrange(0, NUM_STRATEGIES)
-    #                 while rand_idx in players_in_game:
-    #                     rand_idx = random.randrange(0, NUM_STRATEGIES)
-    #                 players_in_game.add(rand_idx)
-    #                 other_stg_nodes = stg_nodes_dict[rand_idx]
-    #                 other_nodes_chosen = other_stg_nodes[map_name]
+                # make other_nodes_chosen
+                play_map_info = map_info_dict[map_name]
+                map_name, map_data, json_dict, n_players, n_seeds = play_map_info
+                # other_nodes_chosen_list = []
+                players_in_game = {stg_idx}
+                for one_player in range(n_players-1):
+                    rand_idx = random.randrange(0, NUM_STRATEGIES)
+                    while rand_idx in players_in_game:
+                        rand_idx = random.randrange(0, NUM_STRATEGIES)
+                    players_in_game.add(rand_idx)
+                    other_stg_nodes = stg_nodes_dict[rand_idx]
+                    other_nodes_chosen = other_stg_nodes[map_name]
 
-    #                 nodes[rand_idx] = [other_nodes_chosen]
-    #                 # other_nodes_chosen_list.append(other_nodes_chosen)
+                    nodes[rand_idx] = [other_nodes_chosen]
+                    # other_nodes_chosen_list.append(other_nodes_chosen)
                     
-    #             # play game
-    #             sim_res = sim.run(json_dict, nodes, 1)
-    #             map_score += get_score(sim_res, stg_idx)
+                # play game
+                sim_res = sim.run(json_dict, nodes, 1)
+                map_score += get_score(sim_res, stg_idx)
 
-    #         rank_dict[stg_idx] = map_score
+            rank_dict[stg_idx] = map_score
 
-    #     # rank_stg_idx_list is a sorted list of stg_idx
-    #     rank_stg_idx_list = sorted(rank_dict, key=rank_dict.get, reverse=True)
-    #     rank_list = [(stg_idx, rank_dict[stg_idx]) for stg_idx in sorted(rank_dict, key=rank_dict.get, reverse=True)]
-    #     for item in rank_list:
-    #         print(item)
+        # rank_stg_idx_list is a sorted list of stg_idx
+        rank_stg_idx_list = sorted(rank_dict, key=rank_dict.get, reverse=True)
+        rank_list = [(stg_idx, rank_dict[stg_idx]) for stg_idx in sorted(rank_dict, key=rank_dict.get, reverse=True)]
+        for idx, score in rank_list:
+            stg = last_step_strategies[idx]
+            print idx, score, stg
 
-
-    #     new_path = os.path.join('training', 'iterations', str(ITE_NUM+1))
-    #     os.makedirs(new_path)
-    #     for i in range(NUM_WINNING):
-    #         stg_idx = rank_stg_idx_list[i]
-    #         stg = last_step_strategies[stg_idx]
-    #         stg.write_to_file(os.path.join(new_path, '{}.json'.format(i)))
-
-
-
-
-    #     # --------------------- Minfa edited ends --------------------
+        new_path = os.path.join('training', 'iterations', str(ITE_NUM+1))
+        if not os.path.exists(new_path):
+            os.makedirs(new_path)
+        for i in range(NUM_WINNING):
+            stg_idx = rank_stg_idx_list[i]
+            stg = last_step_strategies[stg_idx]
+            stg.write_to_file(os.path.join(new_path, '{}.json'.format(i)))
 
 
 
 
+        # --------------------- Minfa edited ends --------------------
 
-    # #     start_time = time.time()
-    # #     # print sim.run(json_dict, nodes, 1)
-    # #     end_time = time.time()
-        
-    # #     print end_time - start_time
-    # #     last_step_strategies = last_step_strategies.values()
-    # #     mutate_group = strategy.generate_mutation_group(last_step_strategies, 32)
-    # # #     for x in mutate_group:
-    # # #         print x
-        
-    # #         
+
